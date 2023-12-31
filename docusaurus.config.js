47ae1c6f287ec8a70af4d5bf4ac02d96503663a7
Isaac Chung <chungisaac1217@gmail.com>
2023-12-31 16:56:05 +0200

    initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,145 @@
+// import type {Config} from '@docusaurus/types';
+// import type * as Preset from '@docusaurus/preset-classic';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+
+const lightCodeTheme = require('prism-react-renderer').themes.github;
+const darkCodeTheme = require('prism-react-renderer').themes.dracula;
+
+const config = {
+  title: 'Tallinn Running Tours',
+  tagline: 'Come run with us in Tallinn and see places that normal tourists do not see!',
+  favicon: 'img/run-icon.png',
+
+  // Set the production url of your site here
+  url: 'https://isaac-chung.github.io',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'isaac-chung', // Usually your GitHub org/user name.
+  projectName: 'runningtourstallinn.github.io', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
+
+  onBrokenLinks: 'warn',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          remarkPlugins: [
+            remarkMath
+          ],
+          rehypePlugins: [rehypeKatex],
+        },
+        blog: {
+          showReadingTime: true,
+          readingTime: ({content, defaultReadingTime}) =>
+            defaultReadingTime({content, options: {wordsPerMinute: 300}}),
+          feedOptions: {
+            type: 'all',
+            copyright: `Copyright © ${new Date().getFullYear()} Isaac Chung`,
+            createFeedItems: async (params) => {
+              const {blogPosts, siteConfig, defaultCreateFeedItems, ...rest} = params;
+              return defaultCreateFeedItems({
+                // Keep only the 10 most recent blog posts in the feed
+                blogPosts: blogPosts.filter((item, index) => index < 20),
+                siteConfig,
+                outDir: rest.outDir,
+              });
+            },
+          },
+          remarkPlugins: [
+            remarkMath
+          ],
+          rehypePlugins: [rehypeKatex],
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+        gtag: {
+          trackingID: 'G-PNHB98RZ0D',
+          anonymizeIP: true,
+        },
+      },
+    ],
+  ],
+  stylesheets: [
+    {
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+      type: 'text/css',
+      integrity:
+        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+      crossorigin: 'anonymous',
+    },
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/1311.jpg',
+    navbar: {
+      title: 'Projects Page',
+      logo: {
+        alt: 'icon',
+        src: 'img/1311.png',
+      },
+      items: [
+        {to: '/', label: 'Isaac Chung', position: 'left'},
+        {
+          href: 'https://github.com/isaac-chung',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+       {
+          title: 'Social',
+          items: [
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/isaacchung1217',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: '/',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/isaac-chung',
+            },
+          ],
+        },
+      ],
+      // Existing copyright...
+      copyright: `Copyright © ${new Date().getFullYear()} Isaac Chung`,
+    },
+    prism: {
+      theme: lightCodeTheme,
+      darkTheme: darkCodeTheme,
+    },
+  },
+};
+
+export default config;
---
+++ src/pages/index.md
@@ -0,0 +1,60 @@
+---
+layout: default
+title: Tallinn Running Tours
+---
+
+## 🏃Tallinn Running Tours 🇪🇪
+
+Come train and explore Tallinn with us!! See places that normal tourists do not see.
+
+Welcome to Estonia! Whether you are a seasoned runner or an occasional park run participant, running is one of the best ways to see a new city. Choose one of the run options below, or contact us if you have further quetsions!
+
+<center><img src="../../static/img/istockphoto-497485376-612x612.jpg" width="50%"/><img src="../../static/img/istockphoto-158837167-612x612.jpg" width="50%"/></center>
+
+<br>
+
+<center>
+👇 Book Now via Whatsapp! 👇
+
+<img src="../../static/img/whatsapp.svg" height="60"/>
+</center>
+
+## The Routes
+Choose one (or more!) of the following popular options based on your available time and fitness level. Most route distances assume we start by the old town gates (Viru Gates).
+
+<center>
+
+|                               |                              |                          |
+|:-----------------------------:|:----------------------------:|:------------------------:|
+|  The Classic <br> 8/10km | Eastbound and Down <br> 10/12km | Hit the Trails <br> 10/15km |
+| Round the Island <br> 12/15/17km | Custom Adventure <br> _Contact us!_ | Private Group <br> _Contact us!_|
+
+</center>
+
+## Logistics
+- **Start time**: This is highly tailored to your needs. Let us know in the booking when you wish to start (in 24-hour clock, e.g. 19:00 will be 7pm) so we can plan accordingly (suject to availability)
+- **Start / Finish location**: Most routes start and finish by the Viru Gates. Let us know in the booking if you wish to start/finish from a different location (e.g. by your hotel, by a bar 🍺 ).
+- **Pace**: This will vary depending on your comfort level. Let us know if you have a pace in mind. During the run, we will check in with you often as well. By default we will aim for 6min/km (10min/mile) pace to start with.
+- **Total time**: This will vary depending on pace and how many stops we take. We will make time for photo stops and fuel stops.
+
+
+## ❗ Important Notes
+1. 💰 While we have a non-tipping policy, feel free to donate to support our back office activities!
+2. 🏃‍♂️ We are local runners and _not_ official tour guides. 
+3. ❄️ Dress for the weather. Please check the weather before leaving your accommodation.
+4. ⚠️ Cancellations: Please allow at least 24-hour notice for any cancellations no matter the reason. Kindly let us know so that we can attend to other runners during your slot.
+5. 🎉 Above all, have fun!
+
+
+<center>
+👇 Book Now via Whatsapp! 👇
+
+<img src="../../static/img/whatsapp.svg" height="60"/>
+</center>
+
+Booking template
+```
+Hello! My name is {NAME} and I would like to join the {OPTION} route.
+```
+
+If you're unsure about anything mentioned above, send us a message and we will get back to you as soon as possible.
